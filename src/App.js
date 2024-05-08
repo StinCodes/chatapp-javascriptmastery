760d1e050086298dbb7009cab1596483fdc2b422
Austin Song <austinhssong@gmail.com>
2024-05-08 10:50:20 -0400

    fixed ReactDOM import, added ChatEngine, created users

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@ import { ChatEngine } from "react-chat-engine";
 import "./App.css";
 
 const App = () => {
-  return <ChatEngine height="100vh" projectID="" userName="" userSecret="" />;
+  return <ChatEngine height="100vh" projectID="2a6998af-01f6-4d6a-a036-313ce094c908
+  " userName="Austin Song" userSecret="1234" />;
 };
 
 export default App;
