045696f3b68ccc75828d90aaec32c3c120704273
Austin Song <austinhssong@gmail.com>
2024-05-10 15:15:24 -0400

    completed MessageForm, added image upload and message upload buttons

--- src/components/MessageForm.jsx
+++ src/components/MessageForm.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { useState } from "react";
 import { sendMessage, isTyping } from "react-chat-engine";
+import { SendOutlined, PictureOutlined } from "@ant-design/icons";
 
 const MessageForm = (props) => {
   const [value, setValue] = useState("");
@@ -18,6 +19,9 @@ const MessageForm = (props) => {
     setValue('')
   };
 
+  const handleUpload = (e)=>{
+    sendMessage(creds, chatId, {files: e.target.files, text:''})
+  }
   return (
     <form className="message-form" onSubmit={handleSubmit}>
       <input
@@ -27,6 +31,15 @@ const MessageForm = (props) => {
         onChange={handleChange}
         onSubmit={handleSubmit}
       />
+      <label htmlFor="upload-button">
+        <span className="image-button">
+          <PictureOutlined className="picture-icon"/>
+        </span>
+      </label>
+      <input type="file" multiple={false} id='upload-button' style={{display: 'none'}} onChange={handleUpload}/>
+      <button type="submit" className="send-button">
+        <SendOutlined className="send-icon"/>
+      </button>
     </form>
   );
 };
